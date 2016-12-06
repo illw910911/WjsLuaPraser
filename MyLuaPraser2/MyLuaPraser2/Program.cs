e1e9e4493891348568ad90c2c937710fa46a3e25
illw910911 <541602485@qq.com>
2016-12-06 23:59:12 +0800

    First test to create thing
    First test to create thing

---
+++ MyLuaPraser2/MyLuaPraser2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Babe.Lua.DataModel;
+
+namespace MyLuaPraser2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string l_str = Console.ReadLine();
+            //Console.WriteLine(l_str);
+            string l_str = "E:\\Work\\LuaUnity\\Assets\\Lua\\View\\Intents\\AdventureIntent.lua";
+            TreeParser l_parser = new TreeParser();
+            l_parser.ParseFile(l_str);
+            
+            for (int i = 0; i < l_parser.File.Tokens.Count; i++)
+            {
+                var l_token = l_parser.File.Tokens[i];
+                Console.WriteLine(l_token.ToString());
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
